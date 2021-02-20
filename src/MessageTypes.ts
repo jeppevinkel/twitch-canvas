8023e501bf705001b5e8699cd663b90a011373a2
Jeppevinkel <jeppevinkel@gmail.com>
2021-02-20 17:09:13 +0100

    Fix emote placement

--- src/MessageTypes.ts
+++ src/MessageTypes.ts
@@ -1,5 +1,32 @@
 export interface IEmote {
-    id: string,
-    start: number,
+    id: string
+    start: number
     end: number
+}
+
+export interface IEmotePosition {
+    index: number
+    id: string
+}
+
+export interface IRect {
+    x: number
+    y: number
+    width: number
+    height: number
+}
+
+export interface IMessage {
+    badges: object[],
+    color: string,
+    displayName: string,
+    "emotes": IEmote[],
+    "flags": string,
+    "mod": boolean,
+    "subscriber": boolean,
+    "tmiSentTs": string,
+    "userType": string,
+    "username": string,
+    "event": string,
+    "messageContent": string
 }
--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,73 @@
 import {createCanvas, Image, loadImage, CanvasRenderingContext2D} from 'canvas';
 import * as fs from "fs";
 import * as path from "path";
-import {IEmote} from './MessageTypes'
+import {IEmote, IEmotePosition, IRect, IMessage} from './MessageTypes'
 
 const testMessage = require('../resources/testMessage.json');
 
 const width = 800;
 const height = 400;
-const fontSize = 34;
-const padding = 10;
+const fontSize = 16;
 
-function getLines(ctx, text, maxWidth) {
-    var words = text.split(" ");
-    var lines = [];
-    var currentLine = words[0];
-
-    for (var i = 1; i < words.length; i++) {
-        var word = words[i];
-        var width = ctx.measureText(currentLine + " " + word).width;
-        if (width < maxWidth) {
-            currentLine += " " + word;
-        } else {
-            lines.push(currentLine);
-            currentLine = word;
-        }
-    }
-    lines.push(currentLine);
-    return lines;
-}
-
-async function drawEmote(ctx: CanvasRenderingContext2D, image: Image, index: number, lineY: number, letterWidth: number) {
-    const dx = padding + letterWidth*index;
-    const dy = lineY - fontSize/2 - letterWidth/2;
-
-    ctx.drawImage(image, dx, dy, letterWidth, letterWidth);
-}
-
-async function drawMessage(message: string) {
-    const canvas = createCanvas(width, height);
-    const ctx = canvas.getContext('2d');
-
-    message = testMessage.messageContent.split('\n').join(' ');
-
-    ctx.font = `${fontSize}pt Lucida Sans Typewriter`;
-
-    const messageWidth = ctx.measureText(message).width;
-
-    const lines = getLines(ctx, message, canvas.width);
-
+async function drawTwitchMessage(ctx: CanvasRenderingContext2D, pos: IRect, message: IMessage) {
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    ctx.fillRect(pos.x, pos.y, pos.width, pos.height);
+    ctx.fillStyle = 'rgba(0, 255, 0, 1)';
     let image = await loadImage('./resources/testEmote.png');
-    let letterWidth: number;
-    letterWidth = messageWidth / message.length;
 
-    let indexOffset = 0;
-    for (let i = 0; i < lines.length; i++) {
-        const lineY = padding + fontSize + (fontSize + padding) * i;
-        ctx.fillText(lines[i], padding, lineY);
-        //if (lines[i].indexOf('M') != -1) drawEmote(ctx, image, lines[i].indexOf('M'), lineY, letterWidth);
+    message.messageContent = message.messageContent.split('\n').join(' ');
 
-        let index = 0;
+    const words = message.messageContent.split(' ');
+    const wordSpacing = ctx.measureText(" ").width;
+    let emotes = testMessage.emotes.sort(((a, b) => a.start - b.start));
 
-        let emotes = testMessage.emotes as IEmote[];
+    let nextEmote = emotes.shift();
 
-        for (let j = 0; j < emotes.length; j++) {
-            let emoteIndex = emotes[j].start - indexOffset;
-            if (emoteIndex >= 0 && emoteIndex <= lines[i].length) {
-                drawEmote(ctx, image, emoteIndex, lineY, letterWidth);
-            }
+    let index = 0;
+    let line = 0;
+    let xPos = pos.x;
+    for (let i = 0; i < words.length; i++) {
+        const wordsWidth = nextEmote.start == index ? fontSize : ctx.measureText(words[i]).width;
+        if (xPos + wordsWidth > pos.x + pos.width) {
+            line++;
+            xPos = pos.x;
         }
 
-        // while(index != -1){
-        //     index = lines[i].indexOf('M', index);
-        //     if (index != -1) {
-        //         drawEmote(ctx, image, index, lineY, letterWidth);
-        //         index++;
-        //     }
-        // }
+        const lineY = pos.y + fontSize + (fontSize + pos.y) * line;
 
-        indexOffset = indexOffset + lines[i].length + 1;
+        if (nextEmote.start == index) {
+
+
+
+            ctx.drawImage(image, xPos, lineY - fontSize, fontSize, fontSize);
+            if (emotes.length) nextEmote = emotes.shift();
+
+            xPos += wordsWidth + wordSpacing;
+            index += words[i].length + 1;
+
+            continue;
+        }
+
+        console.log(words[i] + ", " + xPos + ", " + lineY);
+        ctx.fillText(words[i], xPos, lineY);
+
+        xPos += wordsWidth + wordSpacing;
+        index += words[i].length + 1;
     }
-
-
-
-
-    canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'message.png')));
-
 }
 
-drawMessage("This is a test M perhaps? I guess maybe not? M oh and this line is so long it's probably going to get wrapped! M    RAID!! MMMMMMMM MMMMMM MMM MM M");
+const canvas = createCanvas(width, height);
+const ctx = canvas.getContext('2d');
+
+ctx.font = `${fontSize}pt Lucida Sans Typewriter`;
+
+const msgBox: IRect = {
+    x: 10,
+    y: 10,
+    width: canvas.width - 20,
+    height: canvas.height - 20
+};
+
+drawTwitchMessage(ctx, msgBox, testMessage).then(() => {
+    canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'message.png')));
+});

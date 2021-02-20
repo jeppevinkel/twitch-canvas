bda7a5f63ff376a6890cedd69932fb928f93ad41
Jeppevinkel <jeppevinkel@gmail.com>
2021-02-20 21:29:28 +0100

    Add complete test.

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,12 @@ import {IEmote, IEmotePosition, IRect, IMessage} from './MessageTypes'
 
 const testMessage = require('../resources/testMessage.json');
 
-const width = 800;
-const height = 400;
 const fontSize = 16;
 
 async function drawTwitchMessage(ctx: CanvasRenderingContext2D, pos: IRect, message: IMessage) {
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillRect(pos.x, pos.y, pos.width, pos.height);
-    ctx.fillStyle = 'rgba(0, 255, 0, 1)';
+    //ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    //ctx.fillRect(pos.x, pos.y, pos.width, pos.height);
+    ctx.fillStyle = 'rgba(240, 240, 240, 1)';
     let image = await loadImage('./resources/testEmote.png');
 
     message.messageContent = message.messageContent.split('\n').join(' ');
@@ -33,7 +31,7 @@ async function drawTwitchMessage(ctx: CanvasRenderingContext2D, pos: IRect, mess
             xPos = pos.x;
         }
 
-        const lineY = pos.y + fontSize + (fontSize + pos.y) * line;
+        const lineY = pos.y + fontSize + (fontSize * 1.1) * line;
 
         if (nextEmote.start == index) {
 
@@ -56,18 +54,40 @@ async function drawTwitchMessage(ctx: CanvasRenderingContext2D, pos: IRect, mess
     }
 }
 
-const canvas = createCanvas(width, height);
-const ctx = canvas.getContext('2d');
+async function run() {
+    const notifBg = await loadImage('./resources/notifBg.png');
+    const profile = await loadImage('./resources/47214265.png');
+    const canvas = createCanvas(notifBg.width, notifBg.height);
+    const ctx = canvas.getContext('2d');
+    ctx.drawImage(notifBg, 0, 0);
 
-ctx.font = `${fontSize}pt Lucida Sans Typewriter`;
+    //ctx.font = `${fontSize}pt Lucida Sans Typewriter`;
+    ctx.font = `${fontSize}pt Sans`;
 
-const msgBox: IRect = {
-    x: 10,
-    y: 10,
-    width: canvas.width - 20,
-    height: canvas.height - 20
-};
+    const msgBox: IRect = {
+        x: 125,
+        y: 120,
+        width: 386,
+        height: 129
+    };
 
-drawTwitchMessage(ctx, msgBox, testMessage).then(() => {
-    canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'message.png')));
-});
+    ctx.fillStyle = 'rgba(200, 200, 200, 1)';
+    ctx.font = `26pt Sans`;
+    ctx.fillText(testMessage.displayName, 155, 55, 330);
+    ctx.font = `${fontSize}pt Sans`;
+
+    ctx.beginPath();
+    ctx.arc(64, 64, 55, 0, 2 * Math.PI, false);
+    ctx.closePath();
+
+    ctx.save();
+    ctx.clip();
+    ctx.drawImage(profile, 0, 0, 130, 130);
+    ctx.restore();
+
+    drawTwitchMessage(ctx, msgBox, testMessage).then(() => {
+        canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'message.png')));
+    });
+}
+
+run();
